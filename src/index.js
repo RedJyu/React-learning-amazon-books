ba266c646d5e91250c9510c02316629533cea48b
RedJyu <piotr.stezycki@protonmai.com>
2023-08-23 12:13:19 +0200

    learing props still

--- src/index.js
+++ src/index.js
@@ -10,22 +10,41 @@ const title = 'Iron Flame (The Empyrean, 2)';
 const img =
   'https://images-na.ssl-images-amazon.com/images/I/A1FW1G46+aL._AC_UL900_SR900,600_.jpg';
 
+const firstBook = {
+  author: 'James Clear',
+  title: 'test title',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/81hHy5XrdKL._AC_UL900_SR900,600_.jpg',
+};
+
+const secondBook = {
+  author: 'Rebecca Yarros',
+  title: 'Iron Flame (The Empyrean, 2)',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/A1FW1G46+aL._AC_UL900_SR900,600_.jpg',
+};
 const BookList = () => {
   return (
     <section className='list'>
-      <Book author={author} title={title} img={img} />
-      <Book author={author} title={title} img={img} />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      />
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
     </section>
   );
 };
 
 const Book = (props) => {
-  console.log(props);
+  const { author, title, img } = props;
   return (
     <article className='book'>
-      <img src={props.img} alt={props.title} />
-      <h2>{props.title}</h2>
-      <h4>{props.author}</h4>
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
     </article>
   );
 };
