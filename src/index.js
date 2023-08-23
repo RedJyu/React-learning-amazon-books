1e8a18944597c9c0416f2e348eb4201f3982a5d3
RedJyu <piotr.stezycki@protonmai.com>
2023-08-23 15:02:33 +0200

    spread operator and id

--- src/index.js
+++ src/index.js
@@ -5,20 +5,18 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const author = 'Rebecca Yarros';
-const title = 'Iron Flame (The Empyrean, 2)';
-const img =
-  'https://images-na.ssl-images-amazon.com/images/I/A1FW1G46+aL._AC_UL900_SR900,600_.jpg';
 const books = [
   {
     author: 'James Clear',
     title: 'test title',
     img: 'https://images-na.ssl-images-amazon.com/images/I/81hHy5XrdKL._AC_UL900_SR900,600_.jpg',
+    id: 1,
   },
   {
     author: 'Rebecca Yarros',
     title: 'Iron Flame (The Empyrean, 2)',
     img: 'https://images-na.ssl-images-amazon.com/images/I/A1FW1G46+aL._AC_UL900_SR900,600_.jpg',
+    id: 2,
   },
 ];
 
@@ -31,8 +29,7 @@ const BookList = () => {
   return (
     <section className='list'>
       {books.map((book) => {
-        const { author, title, imf } = book;
-        return <Book {...book} />;
+        return <Book {...book} key={book.id} />;
       })}
     </section>
   );
