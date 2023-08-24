101e60fabca65906bb4773c5acc7615d8107bae8
RedJyu <piotr.stezycki@protonmai.com>
2023-08-24 14:14:02 +0200

    prop drilling working

--- src/index.js
+++ src/index.js
@@ -69,12 +69,15 @@ const BookList = () => {
 // };
 const Book = (props) => {
   const { author, title, img, getBook, id } = props;
+  // const getSingleBook = () => {
+  //   getBook(id);
+  // };
   return (
     <article className='book'>
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
-      <button onClick={getBook}>click</button>
+      <button onClick={() => getBook(id)}>click</button>
     </article>
   );
 };
